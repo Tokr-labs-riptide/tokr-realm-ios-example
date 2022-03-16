55925cc3e015f26c07536e17f4e689b94319836e
Eric McGary <eric@rhove.com>
2022-03-16 11:21:18 -0400

    initial commit

---
+++ RNFTDemo/RNFTDemo/RNFTDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RNFTDemoApp.swift
+//  RNFTDemo
+//
+//  Created by Eric McGary on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct RNFTDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
